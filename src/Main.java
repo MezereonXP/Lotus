44e34e8ec904e724d5dff324f4d9498a1ee2ce16
Ke <mezereonxp@gmail.com>
2018-09-05 22:36:29 +0800

    [UPDATE] Add git ignore with main java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @program: Main
+ * @description: Main class
+ * @author: mezereonxp Email: mezereonxp@gmail.com
+ * @create: 2018/9/5
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
