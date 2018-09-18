1d8026fc62f6e081861ab8a230a10825e6534b23
Ke <mezereonxp@gmail.com>
2018-09-18 19:19:28 +0800

    [UPDATE] Change code

--- src/CountOne.java
+++ src/CountOne.java
@@ -10,16 +10,28 @@ public class CountOne {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int count = 0;
-        for (int i = 1; i <= n; i++) {
-            String s = i + "";
-            char[] chars = s.toCharArray();
-            for (int j = 0; j < chars.length; j++) {
-                if (chars[j] == '1') {
-                    count++;
-                }
+        System.out.println(NumberOf1Between1AndN_Solution(n));
+    }
+
+    public static long NumberOf1Between1AndN_Solution(int n) {
+        long count = 0;
+        long i = 1;
+        long current = 0, after = 0, before = 0;
+
+        while ((n / i) != 0) {
+            before = n / (i * 10);
+            current = (n / i) % 10;
+            after = n - (n / i) * i;
+            if (current > 1) {
+                count = count + (before + 1) * i;
+            } else if (current == 0) {
+                count = count + before * i;
+            } else if (current == 1) {
+                count = count + before * i + after + 1;
             }
+            i = i * 10;
+
         }
-        System.out.println(count);
+        return count;
     }
 }
